e252a49ba633d297e1482ddc176a148b19c8746b
nimo <nico.morloher@mainlevel.at>
2021-04-06 12:59:03 +0200

    initial commit

---
+++ smart_mirror/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Looks cool eh?
+        </p>
+        <p>
+          So I guess you remembered what I said. Very Nice!
+        </p>
+        <p>
+          AND NOW START WORKING ON THE PROJECT!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
